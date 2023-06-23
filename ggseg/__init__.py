9b47232567db5cdebe9a871511a828fc3f6620a1
katrinaCode <116908580+katrinaCode@users.noreply.github.com>
2023-06-23 12:29:29 -0400

    return ax instead of fig

--- ggseg/__init__.py
+++ ggseg/__init__.py
@@ -176,8 +176,8 @@ def plot_dk(data, cmap='Spectral', background='k', edgecolor='w', ylabel='',
 
     # A colorbar is added
     _add_colorbar_(ax, cmap, norm, edgecolor, fontsize*0.75, ylabel)
-
-    return fig
+    print("returning ax")
+    return ax
 
 
 def plot_jhu(data, cmap='Spectral', background='k', edgecolor='w', ylabel='',

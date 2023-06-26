43ee552313d4e13bb8acac2c306a33b99bfb2cef
katrinaCode <116908580+katrinaCode@users.noreply.github.com>
2023-06-26 14:12:50 -0400

    Update __init__.py

--- ggseg/__init__.py
+++ ggseg/__init__.py
@@ -63,7 +63,8 @@ def _add_shared_colorbar_(ax, fig, cmap, norm, vminmax, ec, labelsize, ylabel):
     cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax, 
                         shrink=0.9, location="right",fraction=0.02,
                         label= ylabel, ticks = np.round(np.linspace(vminmax[0], vminmax[1], 5)))
-    cbar.ax.tick_params(labelsize=labelsize, left = True)
+    cbar.ax.tick_params(labelcolor=ec, labelsize=labelsize)
+    cbar.ax.set_ylabel(ylabel, color=ec, fontsize=labelsize, left=True)
 
 def _render_data_(data, wd, cmap, norm, ax, edgecolor):
     import os.path as op
